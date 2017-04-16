45249fa65ac8f44b483a7ca80724f9e089b727b7
tashanzhishi <xingqibing@gmail.com>
2017-04-16 17:38:01 +0800

    区分direcBuffer和非directBuffer

--- src/io_rdma_RdmaChannel.c
+++ src/io_rdma_RdmaChannel.c
@@ -17,7 +17,7 @@ JNIEXPORT void JNICALL Java_io_rdma_RdmaChannel_sendHeader
         (JNIEnv *env, jobject obj, jstring jhost, jint jport, jobject jmsg, jint jlen, jobject jrscb)
 {
     char *host;
-    unsigned char *msg;
+    unsigned char *msg = NULL;
     int len;
 
     jclass ByteBuffer = NULL;
@@ -26,6 +26,7 @@ JNIEXPORT void JNICALL Java_io_rdma_RdmaChannel_sendHeader
     jbyteArray a;
     int pos;
     int port;
+    int needtofree = 0;
 
     host = (*env)->GetStringUTFChars(env, jhost, 0);
     port = jport;
@@ -34,53 +35,53 @@ JNIEXPORT void JNICALL Java_io_rdma_RdmaChannel_sendHeader
 
 
 
-    // 获取array(),position()函数标识
-    ByteBuffer = (*env)->FindClass(env, "java/nio/ByteBuffer");
-    if (NULL == ByteBuffer) {
-        LOG(DEBUG, "find class java.nio.ByteBuffer error.\n");
-        return;
+    msg = (*env)->GetDirectBufferAddress(env, jmsg);
+    if (NULL == msg) {
+        // 获取array(),position()函数标识
+        ByteBuffer = (*env)->FindClass(env, "java/nio/ByteBuffer");
+        if (NULL == ByteBuffer) {
+            LOG(DEBUG, "find class java.nio.ByteBuffer error.\n");
+            return;
+        }
+
+        array = (*env)->GetMethodID(env, ByteBuffer, "array", "()[B");
+        if (NULL == array) {
+            LOG(DEBUG, "find method array error.\n");
+            return;
+        }
+
+        position = (*env)->GetMethodID(env, ByteBuffer, "position", "()I");
+        if (NULL == position) {
+            printf("find method position error.\n");
+            return;
+        }
+
+
+
+        // 使用array(),position()函数获取header的byte[]对象以及pos
+        a = (*env)->CallObjectMethod(env, jmsg, array);
+        if (NULL == a) {
+            LOG(DEBUG, "call method array error.\n");
+            return;
+        }
+
+        pos = (*env)->CallIntMethod(env, jmsg, position);
+        LOG(DEBUG, "position = %d.\n", pos);
+
+
+
+        // 分配空间，并且将数据从java层拷贝到c层
+        len = jlen;
+
+        msg = malloc(len);
+        needtofree = 1;
+        (*env)->GetByteArrayRegion(env, a, pos, len, msg);
     }
-
-    array = (*env)->GetMethodID(env, ByteBuffer, "array", "()[B");
-    if (NULL == array) {
-        LOG(DEBUG, "find method array error.\n");
-        return;
-    }
-
-    position = (*env)->GetMethodID(env, ByteBuffer, "position", "()I");
-    if (NULL == position) {
-        printf("find method position error.\n");
-        return;
-    }
-
-
-
-    // 使用array(),position()函数获取header的byte[]对象以及pos
-    a = (*env)->CallObjectMethod(env, jmsg, array);
-    if (NULL == a) {
-        LOG(DEBUG, "call method array error.\n");
-        return;
-    }
-
-    pos = (*env)->CallIntMethod(env, jmsg, position);
-    LOG(DEBUG, "position = %d.\n", pos);
-
-
-
-    // 分配空间，并且将数据从java层拷贝到c层
-    len = jlen;
-
-    msg = malloc(len);
-    (*env)->GetByteArrayRegion(env, a, pos, len, msg);
-
-    int i;
-    for (i = 0; i < (len > 1024 ? 1024 : len); i++) {
-        LOG(DEBUG, "%c", msg[i]);
-    }
-    LOG(DEBUG, "\n");
 
     send_msg(host, port, msg, len);
-    free(msg);
+    if (needtofree) {
+        free(msg);
+    }
 }
 
 
@@ -100,13 +101,13 @@ JNIEXPORT void JNICALL Java_io_rdma_RdmaChannel_sendHeaderWithBody
     jmethodID array = NULL;
     jmethodID position = NULL;
     jbyteArray ha, ba;
+    int hneedtofree = 0, bneedtofree = 0;
 
     host = (*env)->GetStringUTFChars(env, jhost, 0);
     port = jport;
 
     hlen = jhlen;
     blen = jblen;
-
 
 
     // 获取array(),position()函数标识
@@ -128,39 +129,47 @@ JNIEXPORT void JNICALL Java_io_rdma_RdmaChannel_sendHeaderWithBody
         return;
     }
 
+    header = (*env)->GetDirectBufferAddress(env, jheader);
+    if (NULL == header) {
+        // 使用array(),position()函数获取header的byte[]对象以及pos
+        ha = (*env)->CallObjectMethod(env, jheader, array);
+        if (NULL == ha) {
+            LOG(DEBUG, "call method array error.\n");
+            return;
+        }
 
+        hpos = (*env)->CallIntMethod(env, jheader, position);
+        LOG(DEBUG, "position = %d.\n", hpos);
 
-    // 使用array(),position()函数获取header的byte[]对象以及pos
-    ha = (*env)->CallObjectMethod(env, jheader, array);
-    if (NULL == ha) {
-        LOG(DEBUG, "call method array error.\n");
-        return;
+        header = malloc(hlen);
+        hneedtofree = 1;
+        (*env)->GetByteArrayRegion(env, ha, hpos, hlen, header);
     }
 
-    hpos = (*env)->CallIntMethod(env, jheader, position);
-    LOG(DEBUG, "position = %d.\n", hpos);
 
-    header = malloc(hlen);
-    (*env)->GetByteArrayRegion(env, ha, hpos, hlen, header);
+    body = (*env)->GetDirectBufferAddress(env, jbody);
+    if (NULL == body) {
+        // 使用array(),position()函数获取body的byte[]对象以及pos
+        ba = (*env)->CallObjectMethod(env, jbody, array);
+        if (NULL == ba) {
+            LOG(DEBUG, "call method array error.\n");
+            return;
+        }
 
+        bpos = (*env)->CallIntMethod(env, jbody, position);
+        LOG(DEBUG, "position = %d.\n", bpos);
 
-
-    // 使用array(),position()函数获取body的byte[]对象以及pos
-    ba = (*env)->CallObjectMethod(env, jbody, array);
-    if (NULL == ba) {
-        LOG(DEBUG, "call method array error.\n");
-        return;
+        body = malloc(blen);
+        bneedtofree = 1;
+        (*env)->GetByteArrayRegion(env, ba, bpos, blen, body);
     }
-
-    bpos = (*env)->CallIntMethod(env, jbody, position);
-    LOG(DEBUG, "position = %d.\n", bpos);
-
-    body = malloc(blen);
-    (*env)->GetByteArrayRegion(env, ba, bpos, blen, body);
-
 
 
     send_msg_with_header(host, port, header, hlen, body, blen);
-    free(header);
-    free(body);
+    if (hneedtofree) {
+        free(header);
+    }
+    if (bneedtofree) {
+        free(body);
+    }
 }
